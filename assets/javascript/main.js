363336c3ba501aba36e03f5631e8596afbb6e138
Andrea Velasquez <velasquez189@gmail.com>
2018-04-03 21:05:47 -0400

    push2

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,14 @@
 $(document).ready()
 
+var questions= "";
+
+$(".btn").on("click", function (event) {
+
+    // event.preventDefault();
+    trainName = $("#train-name").val().trim();
+    trainDestination = $("#train-destination").val().trim();
+    trainTime = $("#train-time").val().trim();
+    trainFrequency = $("#train-frequency").val().trim();
+
+    console.log("oh Raj");
+})
