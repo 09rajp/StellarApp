f54b4c2c9c25657c9d7170a8fdc489b44410567b
unknown <jjernigan1065@gmail.com>
2018-04-07 10:37:46 -0400

    fixed timeline

--- assets/javascript/apod.js
+++ assets/javascript/apod.js
@@ -1,1 +1,1 @@
-timeline = new TL.Timeline('timeline-embed', 'https://docs.google.com/spreadsheets/d/1bHLts9pioncEJkvfbVzD6S0q0TaR8TUpFkOGYKYv8Iw/edit#gid=0');
+window.timeline = new TL.Timeline('timeline-embed', 'https://docs.google.com/spreadsheets/d/1bHLts9pioncEJkvfbVzD6S0q0TaR8TUpFkOGYKYv8Iw/edit#gid=0');
